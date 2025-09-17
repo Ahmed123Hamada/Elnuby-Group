b078c62b479ac332e41d1e07c05dd471ae68caff
Ahmed123Hamada <wasdahmed033@gmail.com>
2025-09-17 20:26:29 +0300

    up-services page

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -732,7 +732,7 @@ const Services: React.FC = () => {
                   initial={{ opacity: 0, y: 20 }}
                   animate={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.5 }}
-                  className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8"
+                  className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8"
                 >
                   {currentService.categories.map((category, index) => {
                     const CategoryIcon = category.icon;
